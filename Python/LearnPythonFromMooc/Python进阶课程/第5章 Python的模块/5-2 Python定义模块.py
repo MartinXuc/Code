601f2494a7fdf5c65c7eda6285ba28a285b4ab3a
MartinXuc <xuchicolson@163.com>
2023-10-16 18:47:55 +0800

    2023年10月16日18:47:51

---
+++ Python/LearnPythonFromMooc/Python进阶课程/第5章 Python的模块/5-2 Python定义模块.py
@@ -0,0 +1,3 @@
+# common.py
+def say_hello(name):
+    print('Hello '+name)
---
+++ Python/LearnPythonFromMooc/Python进阶课程/第5章 Python的模块/5-3 Python导入模块.py
@@ -0,0 +1,13 @@
+'''
+import math
+a = math.sin(0)
+b = math.cos(0)
+print(a)
+print(b)
+'''
+
+from math import sin, cos
+a = sin(0)
+b = cos(0)
+print(a)
+print(b)
